de2c8d1f081af506817fd445e389b3d8e2bd07bc
isuli1353 <isuli1353@gmail.com>
2025-03-25 10:30:04 +0900

    Reapply "chore : 필요없는 폰트 파일 삭제"
    This reverts commit c9bfe6616a76269653540d755b1ed8f3393f8b3a.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <span>ProjectLogo</span>
+        <span>Share folio</span>
       </div>
       
       <nav className="nav-center">
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,9 +5,9 @@ const LoginPage = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <div className="login-left">
-          <div className="image-placeholder">이미지 자리</div>
-        </div>
+      <div className="image-placeholder">
+        <img className="login-img" src="/assets/imgs/LoginPage_img.png" alt="로그인 이미지" />
+      </div>
 
         <div className="login-right">
           <h2 className="login-title">로그인</h2>
@@ -15,25 +15,33 @@ const LoginPage = () => {
 
           <div className="input-group">
             <label>아이디</label>
-            <input type="text" placeholder="아이디를 입력하세요" />
+            <input
+              className="login-input"
+              type="text"
+              placeholder="아이디를 입력해주세요."
+            />
           </div>
 
           <div className="input-group">
             <label>비밀번호</label>
-            <input type="password" placeholder="비밀번호를 입력하세요" />
+            <input
+              className="login-input"
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+            />
           </div>
 
           <div className="login-options">
-            <label>
-              <input type="checkbox" /> 아이디 기억하기
+            <label className="remember-me">
+              <input type="checkbox" className="remember-checkbox" /> <span>아이디 기억하기</span>
             </label>
-            <span className="forgot-password">비밀번호를 잊어버리셨나요?</span>
+            <span className="forgot-password">비밀번호를 잃어버리셨나요?</span>
           </div>
 
           <button className="login-btn">로그인</button>
 
           <p className="signup-link">
-            계정이 아직 없으신가요? <Link to="/signup">회원가입</Link>
+            계정이 아직 없으신가요? <Link to="/signup" className="signup-btn">회원가입</Link>
           </p>
         </div>
       </div>
