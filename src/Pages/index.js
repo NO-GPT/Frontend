a083ddf11dc362a165470ca73878b7e2ecf78bb6
isuli1353 <isuli1353@gmail.com>
2025-06-09 04:05:16 +0900

    dev 수정
    변경사항
    - feature/5-MyPage-publishing 브렌치에서 적용되지 않은 사항 적용

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,6 @@
 export { default as MainPage } from './MainPage.jsx';
 export { default as MyPage } from './MyPage.jsx';
+export { default as EditMyPage } from './EditMyPage.jsx';
 export { default as PortfolioPage } from './PortfolioPage.jsx';
 export { default as PortfolioCratePage } from './PortfolioCratePage.jsx';
 export { default as PortfolioDetailPage } from './PortfolioDetailPage.jsx';
